d9e5038dabb748aadb7e5da3e47df22e2f37e299
uyama-hinata <uyama@selva-i.co.jp>
2023-08-08 12:47:09 +0900

    1-2まで実装

--- member_regist.php
+++ member_regist.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+// var_dump($_SESSION);
+
+if(isset($_SESSION['error'])){
+	$error=$_SESSION['error'];
+	// var_dump($error);
+	// die();
+} 
+
+// 次画面へ
+if(!empty($_POST) && !isset($_SESSION['error'])){
+	 header('Location:sent.php');
+	 exit();
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,32 +24,48 @@
 	  <link rel="stylesheet" href="stylesheet.css">
 	</head>
 	<body>
-       <div class="main">
-		    <form action ="sent.php" method ="POST"> 
+	    <div class="main">
+		    <form action ="sent.php" method ="POST" > 
 			    <div class="form-title">会員登録フォーム </div>
 
 			    <div class="form-item"> 
+
 		            <p>氏名
 			        <span>性</span>
-			        <input type="text" name="family-name" required maxlength="20">
-					<?php
-                     if(empty($_POST['family_name'])){
-                     echo "氏名（性）は必須入力です。";
-                     }
-                    ?>
+			        <input type="text" name="family-name" value="<?php if(isset($_SESSION['family-name'])){echo $_SESSION['family-name'];} ?>">
 				    <span>名</span>
-			        <input type="text" name="first-name" required maxlength="20"> 
-					</p>
-					<?php
-                     if(empty($_POST['first_name'])){
-                     echo "氏名（名）は必須入力です。";
-                     }
-                    ?>
+			        <input type="text" name="first-name" value="<?php if(isset($_SESSION['first-name'])){echo $_SESSION['first-name'];} ?>"> 
+                    </p> 
+
+					<!-- 氏名(性)エラー文表示 -->
+					<?php if(isset($error['family-name'])&& in_array("blank", $error['family-name'])):?>
+					<p class="error">氏名(性)は必須です。</p>
+					<?php endif;?>
+					<?php if (isset($error['family-name'])&& in_array("length", $error['family-name'])):?>
+					<p class="error">氏名(性)は20文字以内でお願いします。</p>
+					<?php endif;?>
+					
+
+					<!-- 氏名(名)エラー文表示 -->
+					<?php if (isset($error['first-name'])&& in_array("blank", $error['first-name'])):?>
+					<p class="error">氏名(名)は必須です。</p>
+					<?php endif;?>
+					<?php if (isset($error['first-name'])&& in_array("length", $error['first-name'])):?>
+					<p class="error">氏名(名)は20文字以内でお願いします。</p>
+					<?php endif;?>
+					
+
                 </div>
 
 				<div class="form-item">性別
-					<label><input type="radio" name="gender" value="男性" required>男性</label>
-					<label><input type="radio" name="gender" value="女性">女性</label>
+					<label><input type="radio" name="gender" value="男性" <?php if(!empty($error) && $_SESSION['gender']==="男性"){echo 'checked';}?>>男性</label>
+					<label><input type="radio" name="gender" value="女性" <?php if(!empty($error) && $_SESSION['gender']==="女性"){echo 'checked';}?>>女性</label>
+
+					<!-- 性別エラー文表示 -->
+					<?php if (isset($error['gender'])&& in_array("blank", $error['gender'])):?>
+					<p class="error">性別は必須です。</p>
+					<?php endif;?>
+
                 </div>
 
                 <div class="form-item"> 
@@ -41,46 +75,89 @@
 					 $towns=array('北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県','新潟県','富山県', '石川県', '福井県', '山梨県','長野県','岐阜県','静岡県','愛知県', '三重県','滋賀県','京都府','大阪府','兵庫県','奈良県', '和歌山県','鳥取県','島根県','岡山県', '広島県','山口県', '徳島県','香川県', '愛媛県','高知県', '福岡県','佐賀県', '長崎県', '熊本県', '大分県', '宮崎県','鹿児島県','沖縄県');
 					?>
 					
-					<select name="prefecture" required>
-					 <option value="">選択してください</option>
+					<select name="prefecture">
+					 <option value=""> 選択してください'</option>
 						<?php 
 						 foreach($towns as $town){
-						  echo "<option value='{$town}'>{$town}</option>";
+							if(!empty($error) && ($town===$_SESSION['prefecture'])){echo "<option value='{$_SESSION['prefecture']}' selected>{$town}</option>";}
+						  else{echo "<option value='{$town}'>{$town}</option>";}
 						 }	
 						?>
-                    </select required>
+                    </select>
 					</p>
+
+					<!--住所エラー文表示  -->
+					<?php if (isset($error['prefecture'])&& in_array("blank", $error['prefecture'])):?>
+					<p class="error">住所は必須です。</p>
+					<?php endif;?>
+
 				</div>
 
 				<div class="form-item">それ以降の住所
-					<input type="text" name="address" required maxlength="100">
+					<input type="text" name="address" maxlength="100" value="<?php if(isset($_SESSION['address'])){echo $_SESSION['address'];} ?>">
+
+					<!--住所エラー文表示 -->
+					<?php if (isset($error['address'])&& in_array("length", $error['address'])):?>
+					<p class="error">それ以降の住所は100文字以内でお願いします。</p>
+					<?php endif;?>
+
 				</div>
 
 			    <div class="form-item">パスワード
-			        <input type="password" name="password1" required minlength="8" maxlength="20">
+			        <input type="password" name="password1" value="<?php if(isset($_SESSION['password1'])){echo $_SESSION['password1'];} ?>" minlength="8" maxlength="20">
+
+					<!--パスワードエラー文表示  -->
+					<?php if (isset($error['password1'])&& in_array("blank", $error['password1'])):?>
+					<p class="error">パスワードは必須です。</p>
+					<?php endif;?>
+					<?php if (isset($error['password1'])&& in_array("length", $error['password1'])):?>
+					<p class="error">パスワードは8~20文字以内でお願いします。</p>
+					<?php endif;?>
+
 				</div>
 
 			    <div class="form-item">パスワード確認
-			        <input type="password" name="password2" required minlength="8" maxlength="20">
+			        <input type="password" name="password2" value="<?php if(isset($_SESSION['password2'])){echo $_SESSION['password2'];} ?>"minlength="8" maxlength="20">
+
+					<!--パスワード確認エラー文表示  -->
+					<?php if (isset($error['password2'])&& in_array("blank", $error['password2'])):?>
+					<p class="error">パスワード確認は必須です。</p>
+					<?php endif;?>
+					<?php if (isset($error['password2'])&& in_array("length", $error['password2'])):?>
+					<p class="error">パスワード確認は8~20文字以内でお願いします。</p>
+					<?php endif;?>
+					<?php if (isset($error['password2']) && in_array("difference", $error['password2'])): ?>
+			        <p class="error"> パスワードが上記と違います</p>
+			        <?php endif; ?>
+
 				</div>
 
 			    <div class="form-item">メールアドレス
-			        <input type="text" name="email" required maxlength="200">
-					<?php 
-					$email=$_POST['email'];
-					if(filter_var($email,FILTER_VALIDATE_EMAIL )){
-						echo"$_POST['email']";
-					}else{
-						echo"";
-					}
-					?>
+			        <input type="text" name="email" value="<?php if(isset($_SESSION['email'])){echo $_SESSION['email'];} ?>" maxlength="200">
+
+					<!--メールアドレスエラー文表示  -->
+					<?php if (isset($error['email']) && in_array("blank", $error['email'])): ?>
+			        <p class="error">メールアドレスは必須です</p>
+			        <?php endif; ?>
+
+					<!-- <?php if (isset($error['email']) && in_array("duplicate", $error['email'])): ?> -->
+		            <!-- <p class="error">すでにそのemailは登録されています。</p> -->
+			        <!-- <?php endif; ?> -->
+
+					<?php if (isset($error['email'])&& in_array("length", $error['email'])):?>
+					<p class="error">メールアドレスは200文字以内でお願いします。</p>
+					<?php endif;?>
+
+					<?php if (isset($error['email'])&& in_array("correct", $error['email'])):?>
+					<p class="error">メールアドレスは正しい形式で入力してください。</p>
+					<?php endif;?>
+
 				</div>
 
-			        
-		        <input type="submit" class="btn next" value="確認画面へ">
+		        <input type="submit" class="btn_next" value="確認画面へ" >
 
 	        </form>
-           
+			<?php session_destroy(); ?>
 	   </div>
 	</body>
 </html>
--- sent.php
+++ sent.php
@@ -1,3 +1,107 @@
+<?php
+session_start();
+
+if (!empty($_POST)){
+    // 氏名(性)
+    if ($_POST['family-name']=="") {
+        $error['family-name'][] = 'blank';
+    }
+	elseif (strlen($_POST['family-name']) > 20) {
+        $error['family-name'][] = 'length';
+    }
+
+    //氏名（名）
+	if ($_POST['first-name']==""){
+		$error['first-name'][]='blank';
+	}
+	elseif (strlen($_POST['first-name']) > 20) {
+        $error['first-name'][] = 'length';
+    }
+
+    // 性別
+    if ($_POST['gender']=="") {
+        $error['gender'][] = 'blank';
+    }
+
+	//住所　都道府県　
+	if ($_POST['prefecture']==""){
+		$error['prefecture'][]='blank';
+	}
+
+	//住所　それ以降
+	if (strlen($_POST['address']) > 100) {
+        $error['address'][] = 'length';
+    }
+
+	//パスワード
+	if (empty($_POST['password1'])){
+		$error['password1'][]='blank';
+	}
+	elseif (strlen($_POST['password1'])<8 || strlen($_POST['password1']) > 20) {
+        $error['password1'][] = 'length';
+    }
+    
+    //パスワード確認
+	if (empty($_POST['password2'])){
+		$error['password2'][]='blank';
+	}
+	elseif (($_POST['password1']!= $_POST['password2'])&&($_POST['password2']!="") ){
+        $error['password2'][] = 'difference';
+    }
+
+	// メールアドレス
+    if (empty($_POST['email'])) {
+        $error['email'][] = 'blank';
+    // } else {
+	    // 登録済みメールアドレスではないか
+		// $member = $db->prepare('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
+		// $member->execute(array($_POST['email']));
+		// $record = $member->fetch();
+		// if ($record['cnt'] > 0) {
+			// $error['email'] = 'duplicate';
+	    // }
+	}
+	elseif (strlen($_POST['email']) > 200) {
+        $error['email'] []= 'length';
+    }
+    elseif (!preg_match( '/^[0-9a-z_.\/?-]+@([0-9a-z-]+\.)+[0-9a-z-]+$/', $_POST['email']) ) {
+        $error['email'] []= 'correct';
+    }
+
+
+	if (!empty($error)) {
+        // var_dump($error);
+        $_SESSION['error']= $error;
+        $_SESSION['family-name']=$_POST['family-name'];
+        $_SESSION['first-name']=$_POST['first-name'];
+        $_SESSION['gender']=$_POST['gender'];
+        $_SESSION['prefecture']=$_POST['prefecture'];
+        $_SESSION['address']=$_POST['address'];
+        $_SESSION['password1']=$_POST['password1'];
+        $_SESSION['password2']=$_POST['password2'];
+        $_SESSION['email']=$_POST['email'];
+        // var_dump($_SESSION);
+        // die();
+        header('Location: member_regist.php');
+        exit();
+	}
+
+
+    // history.backで値を引き継げるように
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $_SESSION['form_data'] = $_POST; 
+    }
+    // フォームのデータを取得
+    $form_data = isset($_SESSION['form_data']) ? $_SESSION['form_data'] : array();
+    // セッションのフォームデータを削除（必要に応じて）
+    unset($_SESSION['form_data']);
+
+}
+?>
+
+
+
+
 <!DOCTYPE>
 <html>
     <head>
@@ -26,7 +130,7 @@
                  </div>
 
                  <div class="form-item">パスワード
-                 <?php echo 'セキュリティのため非表示';?>
+                 セキュリティのため非表示
                  </div>
 
                  <div class="form-item">メールアドレス
@@ -35,11 +139,12 @@
 
                 </div>
                 
-                <input type="submit" class="btn next" value="登録完了">
+
+                <input type="submit" class="btn_next" value="登録完了">
                 
             </form>
             
-            <button type="button" class="btn back" onclick=history.back()>前に戻る</button>
+            <input type="button" class="btn_back" value="前へ戻る" onclick=history.back()>
 
         </div>
     </body>
