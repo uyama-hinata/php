53430e63c889e92ebac4eb12cf79b026a5f51152
uyama-hinata <uyama@selva-i.co.jp>
2023-08-29 10:39:53 +0900

    7

--- thread_detail.php
+++ thread_detail.php
@@ -52,14 +52,14 @@ if(!empty($_POST)){
     }
 }
 
-// まず、コメントの総数を取得
-$count_stmt = $db->prepare('SELECT COUNT(*) FROM comments WHERE thread_id = ?');
+// コメントの総数を取得
+$count_stmt = $db->prepare('SELECT COUNT(*) FROM comments WHERE thread_id = ? ');
 $count_stmt->execute([$id]);
 $comment_count = $count_stmt->fetchColumn();
 
 
-// 次に、全てのコメントデータを取得
-$stmt = $db->prepare('SELECT * FROM comments WHERE thread_id = ?');
+// 全てのコメントデータを取得
+$stmt = $db->prepare('SELECT * FROM comments WHERE thread_id = ? ORDER BY created_at ASC');
 $stmt->execute([$id]);
 $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -69,15 +69,28 @@ $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 if ($page <= 0) {
     $page = 1;
 }
-
+// $page = 1 の場合、$offset は (1-1) * 5 = 0 になり、データベースからの取得を最初のレコードから開始することを示す。
+// $page = 2 の場合、$offset は (2-1) * 5 = 5 になり、データベースからの取得を6番目のレコードから開始することを示す。
 $commentsPerPage = 5;
-$offset = ($page - 1) * $commentsPerPage;
+$offset = ($page - 1) * $commentsPerPage; 
 
-// コメントを指定の数だけ取得
-$stmt = $db->prepare('SELECT comments.*, CONCAT(members.name_sei,\' \',members.name_mei) as member_name FROM comments LEFT JOIN members ON comments.member_id = members.id WHERE comments.thread_id = ? LIMIT ? OFFSET ?');
-$stmt->bindParam(1, $id, PDO::PARAM_INT);
-$stmt->bindParam(2, $commentsPerPage, PDO::PARAM_INT);
-$stmt->bindParam(3, $offset, PDO::PARAM_INT);
+// コメントを指定の数だけ取得&「いいね」の情報を取得
+$stmt = $db->prepare('
+    SELECT 
+        comments.*, 
+        CONCAT(members.name_sei,\' \',members.name_mei) as member_name, 
+        (CASE WHEN likes.id IS NULL THEN 0 ELSE 1 END) as is_liked
+    FROM comments 
+    LEFT JOIN members ON comments.member_id = members.id 
+    LEFT JOIN likes AS likes ON comments.id = likes.comment_id AND likes.member_id = ?
+    WHERE comments.thread_id = ? 
+    ORDER BY comments.created_at ASC
+    LIMIT ? OFFSET ?
+');
+$stmt->bindParam(1, $_SESSION['id'], PDO::PARAM_INT);
+$stmt->bindParam(2, $id, PDO::PARAM_INT);
+$stmt->bindParam(3, $commentsPerPage, PDO::PARAM_INT);
+$stmt->bindParam(4, $offset, PDO::PARAM_INT);
 $stmt->execute();
 $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -88,7 +101,71 @@ foreach ($comments as $key => $comment) {
     $comments[$key]['formatted_date'] = $datetime2->format('Y.m.d H:i');
 }
 
+// いいね機能
+if(isset($_POST['likes_id']) && isset($_SESSION['id'])) {
+    $likesId = $_POST['likes_id'];
+    $memberId = $_SESSION['id'];
+
+    // 既にいいねしているか確認
+    $likeCheckStmt = $db->prepare("SELECT * FROM likes WHERE comment_id = ? AND member_id = ?");
+    $likeCheckStmt->execute([$likesId, $memberId]);
+    $likeExists = $likeCheckStmt->fetch(PDO::FETCH_ASSOC);
+
+    if(!$likeExists) {
+        // いいね情報をデータベースに保存
+        $likeStmt = $db->prepare("INSERT INTO likes (comment_id, member_id) VALUES (?, ?)");
+        $likeStmt->execute([$likesId, $memberId]);
+    } else {
+        // いいねを取り消す
+        $unlikeStmt = $db->prepare("DELETE FROM likes WHERE comment_id = ? AND member_id = ?");
+        $unlikeStmt->execute([$likesId, $memberId]);
+    }
+
+    exit();
+}
+
+// 各コメントに対する「いいね」の数をカウントするための配列を作成
+$like_counts = [];
+
+foreach($comments as $comment) {
+    $count_liked = $db->prepare('SELECT COUNT(*) FROM likes WHERE comment_id = ?');
+    $count_liked->execute([$comment['id']]);
+    $like_counts[$comment['id']] = $count_liked->fetchColumn();
+}
+
 ?>
+
+<!-- いいねがクリックされたときの処理-->
+<script>
+    // ログイン状態の確認
+    const isLoggedIn = <?php echo isset($_SESSION['id']) ? 'true' : 'false'; ?>;
+    document.addEventListener( "DOMContentLoaded",function() {
+    document.querySelectorAll('.like-btn').forEach(btn => {
+        btn.addEventListener('click', function() {
+            if (!isLoggedIn) {
+                    window.location.href = "member_regist.php";
+                    return;
+                }
+
+            const commentId = this.dataset.commentId;
+            
+            fetch('?', {
+                method: 'POST',
+                body: new URLSearchParams({ 'likes_id': commentId }),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            })
+            .then(response => response.text())
+            .then(() => {
+                // ここで「いいね」の数を更新するなどの処理を行う
+                location.reload();
+                });
+            });
+        });
+    });
+</script>
+
 
 <!DOCTYPE html>
 <html>
@@ -96,6 +173,7 @@ foreach ($comments as $key => $comment) {
      <meta charset="utf-8">
      <title>スレッド詳細</title>
      <link rel="stylesheet" href="stylesheet.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     </head>
     <body>
         <header>
@@ -144,7 +222,13 @@ foreach ($comments as $key => $comment) {
                     <?php foreach ($comments as $comment): ?>
                         <div class="comment">
                             <?php echo $comment['id'];?>.<?php echo htmlspecialchars($comment['member_name']);?> <?php echo htmlspecialchars($comment['formatted_date'], ENT_QUOTES);?><br>
-                            <?php echo $comment['comment'];?><br>
+                            <?php echo $comment['comment'];?>
+                            
+                            <!-- いいね機能 -->
+                            <button class="like-btn" data-comment-id="<?php echo $comment['id']; ?>">
+                            <span class="fa-solid fa-heart <?php echo $comment['is_liked'] ? 'liked' : ''; ?>"></span>
+                            <span><?php echo $like_counts[$comment['id']];?></span>
+                            </button>
                         </div>
                     <?php endforeach;?>
                 </div>
